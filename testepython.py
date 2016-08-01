4fef16ce471c1f95df949b532d610ad7b4c9b805
leosilvasouza <leonardosilva.souza@yahoo.com.br>
2016-08-01 19:31:51 -0300

    Add files via upload

--- testepython.py
+++ testepython.py
@@ -1,10 +1,10 @@
-# Declarando variáveis
+#-*- coding: utf-8 -*-
 
-export = open("export.json","w") # Criando arquivo que receberá os valores
+#export = open("export.json","w") # Criando arquivo que receberá os valores
 
-hour_initial = str(input("Hour Initial: ex: '00:00' "))
+hour_initial = float("%.2f"%input("Hour Initial: ex: '00.00' "))
 print (hour_initial)
-hour_end = str(input("Hour End: ex: '23:59' "))
+hour_end = float("%.2f"%input("Hour End: ex: '23.59' "))
 print (hour_end)
 minimum_fare_equation = float(input("Minimum Fare Equation: ex: '2.30' "))
 print (minimum_fare_equation)
@@ -20,7 +20,7 @@ waiting_time = bool(input("Waiting time: True or False "))
 print (waiting_time)
 is_active = bool(input("Is Active? "))
 print (is_active)
-typev = str(input("Type: ")) # Variável type precisei declarar como typev
+typev = str(raw_input("Type: ")) # Variável type precisei declarar como typev
 print (typev)
 minimum_speed = int(input("Minimum Speed: ex: '80' "))
 print (minimum_speed)
@@ -28,24 +28,40 @@ minimum_bearing = int(input("Minimum Bearing: ex: '30' "))
 print (minimum_bearing)
 minimum_accuracy = int(input("Minimum Accuracy: ex: '10' "))#Recebo erro informando que precisa ser str e nao int para gravar no json :(
 print (minimum_accuracy)
-export.write("hour_initial: " + hour_initial +"\n" +  
-             "hour_end: " + hour_end + "\n" +
-             "minimum_fare_equation: " + str(minimum_fare_equation) + "\n" +
-             "boarding_fee_equation: " + str(boarding_fee_equation) + "\n" +
-             "price_per_kilometer_equation: " + str(price_per_kilometer_equation) + "\n" +
-             "price_per_minute_equation: " + str(price_per_minute_equation) + "\n" +
-             "rounding_factor_equation: " + str(rounding_factor_equation) + "\n" +
-             "waiting_time: " + str(waiting_time) + "\n" +
-             "is_active: " + str(is_active) + "\n" +
-             "type: " + typev + "\n" +
-             "minimum_speed: " + str(minimum_speed) + "\n" +
-             "minimum_bearing: " + str(minimum_bearing) + "\n" +
-             "minimum_accuracy: " + str(minimum_accuracy)
-             )
 
-export.close()
-print ("dados exportados com sucesso")
+#Iniciando processo de exportacao JSON
 
+import json
+modelo = {
+             "taximeters" : [
+           {
+             "formulas" : [
+                  {
+                  "fare" : "day_fare",
+                  "hour_initial" : hour_initial,
+                  "hour_end" : hour_end,
+                  "equation" : "round(max(12,(boardingFee+(0.3*durationInMinute)+(0.95*distanceInKm))),1)",
+                  "identifier" : "taximeter_day_fare_v1",
+                  "boarding_fee" : boarding_fee_equation
+                  }
+            ],
+            "service" : "regular",
+            "type" : typev,
+            "waiting_time" : waiting_time,
+            "is_active" : is_active,
+            "minimum_speed" : minimum_speed,
+            "minimum_bearing" : minimum_bearing,
+            "minimum_accuracy" : minimum_accuracy,
+            "version" : 1
+           }
+           ]
+      }     
+      
+modelostr = json.dumps(modelo) #convertendo arquivo em str pra exibicao
+modelo = json.loads(modelostr) #transformando a string em estrutura de dados Python
 
+with open('teste.json', 'w') as f:
+      json.dump(modelo, f)
 
-
+with open('teste.json', 'r') as f:
+      modelo = json.load(f)
